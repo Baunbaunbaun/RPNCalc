f6d1f3a68530475c1a546395918342f929088407
Christian Baun <baunbaun@gmail.com>
2014-09-20 22:56:28 +0200

    Initial commit

---
+++ RPNCalc/src/stack/IStack.java
@@ -0,0 +1,12 @@
+package stack;
+
+/**
+* A last-in-first-out collection.
+*/
+public interface IStack<TypeOfValue>
+{
+    public boolean empty();
+    public void push(TypeOfValue value);
+    public TypeOfValue pop() throws EmptyStackException;
+    public TypeOfValue peek() throws EmptyStackException;
+}
